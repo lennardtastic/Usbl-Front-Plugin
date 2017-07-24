96324ad54db5d990bee76a9c41a7ea122d55add3
Lennard <lennard@usabilla.com>
2017-07-24 16:05:02 +0200

    :bug: Fixed the function that adds the other tag

--- usbl-plugin.js
+++ usbl-plugin.js
@@ -72,12 +72,12 @@ function fetchTags() {
   });
 }
 
-
+/**
+ * When a converstation is selected
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
 Front.on('conversation', function (data) {
-  console.log('Conversation', data.conversation);
-  console.log('Contact', data.contact);
-  console.log('Message', data.message);
-  console.log('OtherMessages', data.otherMessages);
   conversation = data.conversation;
   fetchTags();
 });
